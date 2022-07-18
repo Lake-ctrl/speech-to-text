e1f50dc88169bfa1a0d8d5949492142ab98af3c9
HaydenLafourcade <haydenlafourcade@pop-os.localdomain>
2022-07-18 16:34:17 +0100

    Changed logic into separate functions

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
-const axios = require("axios");
-const audioURL = "https://bit.ly/3yxKEIY";
-const refreshInterval = 5000;
+const axios = require("axios").default;
 const API_KEY = process.env.API_KEY;
+
+const form = document.getElementById("form");
+const button = document.getElementById("button");
 
 // Setting up the AssemblyAI headers
 const assembly = axios.create({
@@ -14,28 +15,48 @@ const assembly = axios.create({
 	},
 });
 
-const getTranscript = async () => {
-	// Sends the audio file to AssemblyAI for transcription
-	const response = await assembly.post("/transcript", {
-		audio_url: audioURL,
-	});
+async function uploadAudio() {
+	const formPayload = new FormData();
+	const file = document.getElementById("file");
+	const audioFile = file.files[0];
+	formPayload.append("audio", audioFile);
 
-	// Interval for checking transcript completion
-	const checkCompletionInterval = setInterval(async () => {
-		const transcript = await assembly.get(
-			`/transcript/${response.data.id}`
-		);
-		const transcriptStatus = transcript.data.status;
+	try {
+		const uploadData = await assembly.post("/upload", formPayload, {
+			headers: {
+				"Transfer-Encoding": "chunked",
+			},
+		});
+		return uploadData;
+	} catch (err) {
+		console.error(err);
+	}
+}
 
-		if (transcriptStatus !== "completed") {
-			console.log(`Transcript Status: ${transcriptStatus}`);
-		} else if (transcriptStatus === "completed") {
-			console.log("\nTranscription completed!\n");
-			let transcriptText = transcript.data.text;
-			console.log(`Your transcribed text:\n\n${transcriptText}`);
-			clearInterval(checkCompletionInterval);
-		}
-	}, refreshInterval);
-};
+async function getTranscription(audio_URL) {
+	try {
+		const transcription = await assembly.post("/transcript", {
+			audio_url: audio_URL,
+		});
+	} catch (err) {
+		console.error(err);
+	}
+}
 
-getTranscript();
+async function handleClick(e) {
+	e.preventDefault();
+	try {
+		const audio_URL = await uploadAudio();
+		const transcription = await getTranscription(audio_url);
+		return transcription;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+function helloButton() {
+	console.log("Hello");
+}
+
+form.addEventListener("submit", handleClick(e));
+button.addEventListener("click", helloButton);
